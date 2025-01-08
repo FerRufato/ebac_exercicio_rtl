cc89f8be52345dda4c4c313f0832ad8e81483c06
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2025-01-08 04:09:39 -0300

    Adicionado teste de inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from './index'; 
 
-describe('Teste para o componente PostComment', () => {
+
+describe('Teste para o componente Post', () => {    
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        render(<Post />);
+        expect(screen.getByText('Comentar')).toBeInTheDocument(); 
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<Post />); 
+
+        const textarea = screen.getByTestId('comment-textarea'); 
+        const button = screen.getByTestId('submit-button'); 
+        
+      
+        fireEvent.change(textarea, { target: { value: 'Este é o primeiro comentário!' } });        
+      
+        fireEvent.click(button);
+        
+  
+        expect(screen.getByText('Este é o primeiro comentário!')).toBeInTheDocument();
+ 
+        fireEvent.change(textarea, { target: { value: 'Este é o segundo comentário!' } });
+        
+  
+        fireEvent.click(button);        
+
+        expect(screen.getByText('Este é o segundo comentário!')).toBeInTheDocument();
     });
 });
